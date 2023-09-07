b12fae40806440acde9389f2b89f12c94c13c3c1
Abdoulaye Diallo <aslimdiallo@gmail.com>
2023-09-07 14:06:43 +0000

    some changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     <div className="h-[100vh] w-full flex items-center justify-center">
       <div className="text-center">
       <div className="text-lg font-light text-[#0d1872] mb-4">Bienvenue</div>
-      <div className='text-5xl font-light text-[#0d1872]'>Stage Guinée</div>
+      <div className='text-5xl font-bold text-[#0d1872]'>Stage Guinée</div>
       </div>
     </div>
   )
