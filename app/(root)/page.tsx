1f02a443627f1795874b76df9d07dd30533799af
Abdoulaye Diallo <aslimdiallo@gmail.com>
2023-09-08 15:26:28 +0000

    clerk and shadcn config

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs/app-beta";
 
-export default function Home() {
+const Home= () => {
   return (
     <div className="h-[100vh] w-full flex items-center justify-center">
       <div className="text-center">
       <div className="text-lg font-light text-[#0d1872] mb-4">Bienvenue</div>
       <div className='text-5xl font-bold text-[#0d1872]'>Stage Guinée</div>
+      <UserButton afterSignOutUrl="/" />
       </div>
     </div>
   )
 }
+
+export default Home;
