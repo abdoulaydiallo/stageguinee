c71e6d98481f924bd0547e434d9738c406d60b15
Abdoulaye Diallo <aslimdiallo@gmail.com>
2023-09-15 10:51:19 +0000

    creating profile ui - changes

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import Image from "next/image";
-import Link from "next/link";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import MainNav from "./main-nav";
 import { useState } from "react";
